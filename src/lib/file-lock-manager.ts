df615852bc53743e62f899d2c886691873d44723
Jordy <jordy.vervloed@gmail.com>
2024-03-17 12:03:19 +0100

    Dev (#1)
    * feat: initial implementation
    
    ---------
    
    Co-authored-by: Jordy Vervloed <jvervloed@rgfstaffing.nl>

---
+++ src/lib/file-lock-manager.ts
@@ -0,0 +1,33 @@
+import * as vscode from 'vscode';
+
+let masterLock = false;
+let poFilesLocks = 0;
+
+export function setMasterLock(value: boolean): void {
+  masterLock = value;
+  vscode.window.showInformationMessage(
+    `Masterlock: ${masterLock ? 'enabled' : 'disabled'}`
+  );
+  console.log(`Set: Masterlock: ${masterLock ? 'enabled' : 'disabled'}`);
+}
+
+//TODO: Rename to FileWatchLock? Since it blocks the file watchers from reacting to changes. Instead of not being able to write to the files.
+export function addPoFilesLock(): void {
+  poFilesLocks++;
+  console.log(`Added 1. Po File Locks Active: ${poFilesLocks}`);
+}
+
+export function removePoFileLock(): void {
+  poFilesLocks--;
+  console.log(`Removed 1. Po File Locks Active: ${poFilesLocks}`);
+}
+
+export function arePoFilesLocked(): boolean {
+  console.log(`Po files locked: ${masterLock || poFilesLocks > 0}`);
+  return masterLock || poFilesLocks > 0;
+}
+
+export function isMasterLockEnabled(): boolean {
+  // console.log(`Masterlock: ${masterLock ? 'enabled' : 'disabled'}`);
+  return masterLock;
+}
---
+++ todo.md
@@ -0,0 +1,4 @@
+# TODO
+
+- iets bedenken for Save-All. Bij meerdere saves tegelijkertijd afvangen dat er niet net zoveel tasks worden afgetrapt.
+-
