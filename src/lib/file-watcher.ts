1244798bd1c90686c5043aa3f6197748fde950f0
Jordy <jordy.vervloed@gmail.com>
2024-03-21 09:34:18 +0100

    feat: handle merge state correctly (#30)
    * feat: handle merge state correctly
    
    Also added some buttons for manual action triggers.

--- src/lib/file-watcher.ts
+++ src/lib/file-watcher.ts
@@ -2,14 +2,14 @@ import * as vscode from 'vscode';
 
 export function createFileWatcher(
   pattern: vscode.GlobPattern,
-  onChange: (uri: vscode.Uri) => void,
+  onChange: (fsPath: string) => void,
   ...disableFlags: (() => boolean)[]
 ): vscode.FileSystemWatcher {
   const watcher = vscode.workspace.createFileSystemWatcher(pattern);
 
   watcher.onDidChange((uri) => {
     if (!disableFlags.some((disableFlag) => disableFlag())) {
-      onChange(uri);
+      onChange(uri.fsPath);
     }
   });
 
